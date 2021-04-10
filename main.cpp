09f5878a34e91804b9f130df1b755172822de311
Piyush <piyuinshort@gmail.com>
2021-04-10 12:56:50 +0530

    basic template

---
+++ main.cpp
@@ -0,0 +1,66 @@
+/*-----------------. .----------------. .----------------. .----------------. .----------------. 
+| .--------------. | .--------------. | .--------------. | .--------------. | .--------------. |
+| |   ______     | | |  _________   | | | _____  _____ | | |  ____  ____  | | |  ____  ____  | |
+| |  |_   __ \   | | | |_   ___  |  | | ||_   _||_   _|| | | |_  _||_  _| | | | |_   ||   _| | |
+| |    | |__) |  | | |   | |_  \_|  | | |  | | /\ | |  | | |   \ \  / /   | | |   | |__| |   | |
+| |    |  ___/   | | |   |  _|  _   | | |  | |/  \| |  | | |    > `' <    | | |   |  __  |   | |
+| |   _| |_      | | |  _| |___/ |  | | |  |   /\   |  | | |  _/ /'`\ \_  | | |  _| |  | |_  | |
+| |  |_____|     | | | |_________|  | | |  |__/  \__|  | | | |____||____| | | | |____||____| | |
+| |              | | |              | | |              | | |              | | |              | |
+| '--------------' | '--------------' | '--------------' | '--------------' | '--------------' |
+ '----------------' '----------------' '----------------' '----------------' '----------------*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef map<int,int> mii;
+typedef pair<int,int> pii; 
+typedef vector<pii> vpii; 
+
+#define int ll
+#define F first 
+#define S second
+#define endl "\n"
+#define MP make_pair
+#define PB push_back
+#define WL(t) while(t--) 
+#define SZ(x) ((int)(x).size()) 
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define REP(i,n) for (int i = 0; i < n;i++) 
+#define FOR(i,a,b) for (int i = a; i < b; i++) 
+#define FORD(i,a,b) for (int i = a; i >= b; i--)
+#define REPD(i,n) for (int i = n-1; i >= 0;  i--) 
+#define PRECISION(x) cout << fixed << setprecision(x);  
+#define inrange(i,a,b) ((i>=min(a,b)) && (i<=max(a,b))) 
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+
+
+const int MAXN = 1000005; 
+const int MOD = 1000000007; 
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));} 
+
+ 
+// typedef unsigned long long int ulli;
+
+//                                               <<<  WRITE CODE HERE >>>
+
+void solve()
+{   
+   
+}
+signed main()
+{   
+   FAST_IO
+    int t=1;
+    // cin>>t;    // UNCOMMENT THIS LINE, WHEN MULTIPLE TEST CASES
+    WL(t){ solve(); cout<<endl; }
+    return 0;
+}
+
